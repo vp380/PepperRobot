198936839b5d1591e6b2a1353b59e4663c7ef354
vp380 <vasileperciuleac@gmail.com>
2019-11-06 13:58:05 +0000

    "First commit."

---
+++ app/src/main/java/com/example/pepperrobot/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.pepperrobot;
+
+import android.os.Bundle;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        Say say = SayBuilder.with(qiContext) // Create a builder with the QiContext.
+                .withText("\\style=joyful\\Hello human! I know this voice is supposed to be reassuring, but it's actually intensely creepy!") // Specify the action parameters.
+                .build();
+        say.run();
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // The robot focus is lost.
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // The robot focus is refused.
+    }
+
+}
